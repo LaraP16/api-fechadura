814cff1f539559cdf52c1201b649c4110cabf144
LaraP16 <laracspuga@gmail.com>
2024-08-23 14:25:40 -0300

    Commit Inicial
    Commit Inicial

---
+++ Controllers/UsuariosControllers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Controlers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Usuarios : ControllerBase
+    {
+        
+    }
+}
