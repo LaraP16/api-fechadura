1134e8886e769ba8ed7d023658d9107d6071376e
LaraP16 <laracspuga@gmail.com>
2024-08-23 17:09:34 -0300

    aaaaa

--- Controllers/UsuariosControllers.cs
+++ Controllers/UsuariosControllers.cs
@@ -1,15 +1,21 @@
+using System.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api_fechadura.DAO;
+using api_fechadura.Models;
+using ZstdSharp.Unsafe;
+using System.Xml.Serialization;
 
-namespace Usuarios.Controlers
+namespace api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class Usuarios : ControllerBase
     {
-        
+        private UsuariosDAO _usuariosDAO;
+
     }
 }
--- DAO/UsuariosDAO.cs
+++ DAO/UsuariosDAO.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api_fechadura.Models;
-//using api_fechadura.Repository;
-//using MySql.Data.MySqlClient;
+using api_fechadura.Repository;
+using MySql.Data.MySqlClient;
 
 namespace api_fechadura.DAO
 {
     public class UsuariosDAO
     {
-         //private MySqlConnection _connection;
+         private MySqlConnection _connection;
 
          public UsuariosDAO()
          {
-            //_connection = MySqlConnectionFactory.GetConnection();
+            _connection = MySqlConnectionFactory.GetConnection();
          }
 
-         public<Usuario> usuarios = new List<Usuario>();
-         string query = "SELECT * FROM Usuario";
+         public List<Usuario> GetAll()
+         {
+             List<Usuario> usuarios = new List<Usuario>();
+             string query = "SELECT * FROM usuario";
 
-         try
-         {
-            _connection.Open();
-            MySqlCommand command = new MySqlCommand(query, _connection);
-            using (MySqlDataReader reader = command.ExecuteReader())
-            {
-                
-            }
-         }
-         catch (System.Exception)
-         {
-            
-            throw;
+             try
+             {
+               _connection.Open();
+               MySqlCommand command = new MySqlCommand(query, _connection);
+               using(MySqlDataReader reader = command.ExecuteReader())
+               {
+                  while (reader.Read())
+                  {
+                     Usuario usuario = new Usuario();
+                     usuario.IdUsuario = reader.GetInt32("idusuario");
+                     usuario.Nome = reader.GetString("nome");
+
+                  }
+               }
+             }
          }
     }
 }
