591ba10cc0352ffddfb807a7dd8a909d9b8678c1
LaraP16 <laracspuga@gmail.com>
2024-08-23 14:39:11 -0300

    p

--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace api_eventos.Models
+namespace api_fechadura.Models
 {
     public class Usuario
     {
