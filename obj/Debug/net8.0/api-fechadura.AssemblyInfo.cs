38d082c9ddc5f2af3b2f7b65682950a53426d32c
LaraP16 <laracspuga@gmail.com>
2024-08-28 16:52:54 -0300

    asdfg

--- obj/Debug/net8.0/api-fechadura.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-fechadura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-fechadura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4275fc599c1ac5c3887e4774e19c726465e4bf73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661a7df63dffa25415494ae4abad6d2c1238c306")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-fechadura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-fechadura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
